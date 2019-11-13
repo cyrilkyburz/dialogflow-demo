957d9476dcac9afe3d739e1ac8510db9ff6c8884
Cyril Kyburz <c@kyburz.me>
2019-11-13 12:43:39 +0100

    Updates

---
+++ precache-manifest.cafd1d1932c51cd4d5f6690a1a0acabe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9aaa73a4cca0368a1a7286a994a55a",
+    "url": "http://cyrilkyburz.ch/dialogflow-demo/index.html"
+  },
+  {
+    "revision": "29953e02ec905fbf6c75",
+    "url": "http://cyrilkyburz.ch/dialogflow-demo/static/css/main.81c34cda.chunk.css"
+  },
+  {
+    "revision": "b40c955d31eed12d1ac4",
+    "url": "http://cyrilkyburz.ch/dialogflow-demo/static/js/2.cac71159.chunk.js"
+  },
+  {
+    "revision": "29953e02ec905fbf6c75",
+    "url": "http://cyrilkyburz.ch/dialogflow-demo/static/js/main.9e3d4148.chunk.js"
+  },
+  {
+    "revision": "31997120be4ff5774355",
+    "url": "http://cyrilkyburz.ch/dialogflow-demo/static/js/runtime-main.666c92f8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.437257988fa9113cad048b16584a554b.js"
+  "http://cyrilkyburz.ch/dialogflow-demo/precache-manifest.cafd1d1932c51cd4d5f6690a1a0acabe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://cyrilkyburz.ch/dialogflow-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
